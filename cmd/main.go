c3ccef8c6bbe72ef097d6c6e0ccec9f093cf660d
elina.biktimirova <green_elina@mail.ru>
2025-11-20 13:11:43 +0300

    add (conf): load conf

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"github.com/konnen/review-assign-service/internal/config"
+	"os"
+)
 
+func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		fmt.Printf("failed to load config: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(cfg)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+type Config struct {
+	ServerPort string
+	DB_url     string
+}
+
+func LoadConfig() (*Config, error) {
+	const pth = "config.LoadConfig"
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", pth, err)
+	}
+	dbUrl := getEnv("DATABASE_URL", "")
+	if dbUrl == "" {
+		return nil, fmt.Errorf("DATABASE_URL is not set in .env")
+	}
+
+	return &Config{
+		ServerPort: getEnv("SERVER_PORT", ":8080"),
+		DB_url:     dbUrl,
+	}, nil
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
