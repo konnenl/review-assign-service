f7d722fbe7207256d3da09a393319ea7c157b719
elina.biktimirova <green_elina@mail.ru>
2025-11-20 13:20:53 +0300

    add (log): init logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"github.com/konnen/review-assign-service/internal/config"
 	"os"
 )
 
 func main() {
+	lgr := slog.New(slog.NewJSONHandler(os.Stderr, nil))
 	cfg, err := config.LoadConfig()
 	if err != nil {
-		fmt.Printf("failed to load config: %v\n", err)
+		lgr.Error("failed to load config", "error", err)
 		os.Exit(1)
 	}
-	fmt.Println(cfg)
+	lgr.Info("config loaded", "cfg", cfg)
 }
